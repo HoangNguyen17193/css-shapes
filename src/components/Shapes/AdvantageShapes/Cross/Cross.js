e12767261556ed6e2b6a8e34926c3738c4d5bd7e
hoangnguyen17193 <hoangnh17193@gmail.com>
2020-11-06 15:44:16 +0700

    add Cross

---
+++ src/components/Shapes/AdvantageShapes/Cross/Cross.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Cross.module.scss';
+import Shape from '../../Shape';
+
+const Cross = () => (
+    <Shape title='Cross'>
+        <div className={classes.Cross}>
+        </div>
+    </Shape>
+);
+
+export default Cross;
