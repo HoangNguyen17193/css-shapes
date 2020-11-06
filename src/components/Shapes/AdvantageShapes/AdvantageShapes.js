d6a0c5d1e69d3a7c87ba6a02517ce4863892158a
hoangnguyen17193 <hoangnh17193@gmail.com>
2020-11-06 15:45:58 +0700

    add shapes

--- src/components/Shapes/AdvantageShapes/AdvantageShapes.js
+++ src/components/Shapes/AdvantageShapes/AdvantageShapes.js
@@ -6,6 +6,9 @@ import Infinity from './Infinity/Infinity';
 import Leaf from './Leaf/Leaf';
 import Flag from './Flag/Flag';
 import Heart from './Heart/Heart';
+import PacMan from './PacMan/PacMan';
+import Egg from './Egg/Egg';
+import Cross from './Cross/Cross';
 import classes from './AdvantageShapes.module.scss';
 
 const AdvantageShapes = () => (
@@ -15,6 +18,9 @@ const AdvantageShapes = () => (
         <Col className={classes.Column} span={24} lg={{span: 8}}><Infinity/></Col>
         <Col className={classes.Column} span={24} lg={{span: 8}}><Leaf/></Col>
         <Col className={classes.Column} span={24} lg={{span: 8}}><Flag/></Col>
+        <Col className={classes.Column} span={24} lg={{span: 8}}><Cross/></Col>
+        <Col className={classes.Column} span={24} lg={{span: 8}}><PacMan/></Col>
+        <Col className={classes.Column} span={24} lg={{span: 8}}><Egg/></Col>
         <Col className={classes.Column} span={24} lg={{span: 8}}><Heart/></Col>
     </Row>
 );
