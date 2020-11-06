2934f2f81f4736c51ab4c39a88e73f85e526c92f
hoangnguyen17193 <hoangnh17193@gmail.com>
2020-11-06 15:30:45 +0700

    add PacMan

---
+++ src/components/Shapes/AdvantageShapes/PacMan/PacMan.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from './PacMan.module.scss';
+import Shape from '../../Shape';
+
+const PacMan = () => (
+    <Shape title="Pac Man">
+        <div className={classes.PacMan}>
+        </div>
+    </Shape>
+
+);
+
+export default PacMan;
