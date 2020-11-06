a6ff9a9e17bfd79ebd03da140d95114c0c234a7c
Hoang Nguyen <hoangnh17193@gmail.com>
2020-11-06 15:05:11 +0700

    add Flag

---
+++ src/components/Shapes/AdvantageShapes/Flag/Flag.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Flag.module.scss';
+import Shape from '../../Shape';
+
+const Infinity = () => (
+    <Shape title='Infinity'>
+        <div className={classes.Flag}>
+        </div>
+    </Shape>
+);
+
+export default Infinity;
