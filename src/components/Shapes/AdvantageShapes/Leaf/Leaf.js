ab42d0f3275715ea09ee0ae1331842f7390d7fd9
Hoang Nguyen <hoangnguyenhh@lifung.com>
2020-11-06 14:59:19 +0700

    add Leaf

---
+++ src/components/Shapes/AdvantageShapes/Leaf/Leaf.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from './Leaf.module.scss';
+import Shape from '../../Shape';
+
+const Infinity = () => (
+    <Shape title="Leaf">
+        <div className={classes.Leaf}>
+        </div>
+    </Shape>
+
+);
+
+export default Infinity;
