70510e9ac1a1d03d4a4f5a30c84e4a0302bd29a8
hoangnguyen17193 <hoangnh17193@gmail.com>
2020-11-06 15:15:41 +0700

    add Heart

---
+++ src/components/Shapes/AdvantageShapes/Heart/Heart.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from './Heart.module.scss';
+import Shape from '../../Shape';
+
+const Infinity = () => (
+    <Shape title="Leaf">
+        <div className={classes.Heart}>
+        </div>
+    </Shape>
+
+);
+
+export default Infinity;
