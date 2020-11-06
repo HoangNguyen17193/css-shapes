db861df1e986ef261950aa111ffb87b06239582e
Hoang Nguyen <hoangnguyenhh@lifung.com>
2020-11-06 14:59:05 +0700

    add Infinity

---
+++ src/components/Shapes/AdvantageShapes/Infinity/Infinity.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Infinity.module.scss';
+import Shape from '../../Shape';
+
+const Infinity = () => (
+    <Shape title='Infinity'>
+        <div className={classes.Infinity}>
+        </div>
+    </Shape>
+);
+
+export default Infinity;
