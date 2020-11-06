99358b4e2e262a93b97d239f34c1aac6571b1908
Hoang Nguyen <hoangnguyenhh@lifung.com>
2020-11-06 15:00:06 +0700

    update main page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, {Component} from 'react';
 import MainLayout from "./components/Common/MainLayout/MainLayout";
+import AdvantageShapes from "./components/Shapes/AdvantageShapes/AdvantageShapes";
 
 class App extends Component {
     render() {
         return (
-            <MainLayout>Hello</MainLayout>
+            <MainLayout>
+                <AdvantageShapes />
+            </MainLayout>
         )
     }
 }
