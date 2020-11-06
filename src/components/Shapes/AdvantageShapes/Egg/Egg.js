8db7775b4d84999a8176961a9d03050ea563f5fd
hoangnguyen17193 <hoangnh17193@gmail.com>
2020-11-06 15:31:00 +0700

    add Egg

---
+++ src/components/Shapes/AdvantageShapes/Egg/Egg.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './Egg.module.scss';
+import Shape from '../../Shape';
+
+const Egg = () => (
+    <Shape title='Egg'>
+        <div className={classes.Egg}>
+        </div>
+    </Shape>
+);
+
+export default Egg;
