05ded0f8747f85eeac3c5ad2fda51a49901583d6
hoangnguyen17193 <hoangnh17193@gmail.com>
2020-11-06 15:16:14 +0700

    display flag

--- src/components/Shapes/AdvantageShapes/AdvantageShapes.js
+++ src/components/Shapes/AdvantageShapes/AdvantageShapes.js
@@ -4,6 +4,7 @@ import CurvedArrow from './CurvedArrow/CurvedArrow';
 import PointerArrow from './PointerArrow/PointerArrow';
 import Infinity from './Infinity/Infinity';
 import Leaf from './Leaf/Leaf';
+import Flag from './Flag/Flag';
 import classes from './AdvantageShapes.module.scss';
 
 const AdvantageShapes = () => (
@@ -12,6 +13,7 @@ const AdvantageShapes = () => (
         <Col className={classes.Column} span={24} lg={{span: 8}}><PointerArrow/></Col>
         <Col className={classes.Column} span={24} lg={{span: 8}}><Infinity/></Col>
         <Col className={classes.Column} span={24} lg={{span: 8}}><Leaf/></Col>
+        <Col className={classes.Column} span={24} lg={{span: 8}}><Flag/></Col>
     </Row>
 );
 
