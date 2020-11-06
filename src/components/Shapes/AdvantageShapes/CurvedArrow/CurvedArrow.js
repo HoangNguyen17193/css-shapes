2de0ccda913dae3679a5a6db642bb9e5a0c36d9f
Hoang Nguyen <hoangnguyenhh@lifung.com>
2020-11-06 14:57:23 +0700

    add CurvedArrow

---
+++ src/components/Shapes/AdvantageShapes/CurvedArrow/CurvedArrow.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from './CurvedArrow.module.scss';
+import Shape from '../../Shape';
+
+const CurvedArrow = () => (
+    <Shape title='Curved Arrow'>
+        <div className={classes.CurvedArrow}>
+        </div>
+    </Shape>
+
+);
+
+export default CurvedArrow;
