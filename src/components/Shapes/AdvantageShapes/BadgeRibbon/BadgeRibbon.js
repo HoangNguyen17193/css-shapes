721284f18ee01dacd16268d578c0a259ba3b3617
hoangnguyen17193 <hoangnh17193@gmail.com>
2020-11-09 10:49:49 +0700

    add badge ribbon

---
+++ src/components/Shapes/AdvantageShapes/BadgeRibbon/BadgeRibbon.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './BadgeRibbon.module.scss';
+import Shape from '../../Shape';
+
+const BadgeRibbon = () => (
+    <Shape title='Badge Ribbon'>
+        <div className={classes.BadgeRibbon}>
+        </div>
+    </Shape>
+);
+
+export default BadgeRibbon;
