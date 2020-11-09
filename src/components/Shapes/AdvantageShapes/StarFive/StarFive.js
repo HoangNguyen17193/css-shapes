ff0f58fe23f0e2e482d0cd4097699f4dc0d1d8e0
hoangnguyen17193 <hoangnh17193@gmail.com>
2020-11-09 10:49:33 +0700

    add star

---
+++ src/components/Shapes/AdvantageShapes/StarFive/StarFive.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './StarFive.module.scss';
+import Shape from '../../Shape';
+
+const StarFive = () => (
+    <Shape title='Star (5points)'>
+        <div className={classes.StarFive}>
+        </div>
+    </Shape>
+);
+
+export default StarFive;
