fc0ac6fe41ea803ace78df60f0f8ab00c598f743
hoangnguyen17193 <hoangnh17193@gmail.com>
2020-11-09 11:01:59 +0700

    update shapes

--- src/components/Shapes/AdvantageShapes/AdvantageShapes.js
+++ src/components/Shapes/AdvantageShapes/AdvantageShapes.js
@@ -15,6 +15,7 @@ import YinYang from './YinYang/YinYang';
 import Moon from './Moon/Moon';
 import StarFive from './StarFive/StarFive';
 import BadgeRibbon from './BadgeRibbon/BadgeRibbon';
+import TVScreen from './TVScreen/TVScreen';
 
 import classes from './AdvantageShapes.module.scss';
 
@@ -23,18 +24,22 @@ const AdvantageShapes = () => (
         <Col className={classes.Column} span={24} lg={{span: 8}}><CurvedArrow/></Col>
         <Col className={classes.Column} span={24} lg={{span: 8}}><PointerArrow/></Col>
         <Col className={classes.Column} span={24} lg={{span: 8}}><Infinity/></Col>
-        <Col className={classes.Column} span={24} lg={{span: 8}}><Moon/></Col>
+
+        <Col className={classes.Column} span={24} lg={{span: 8}}><TVScreen/></Col>
         <Col className={classes.Column} span={24} lg={{span: 8}}><Flag/></Col>
         <Col className={classes.Column} span={24} lg={{span: 8}}><Cross/></Col>
+
         <Col className={classes.Column} span={24} lg={{span: 8}}><PacMan/></Col>
         <Col className={classes.Column} span={24} lg={{span: 8}}><Egg/></Col>
         <Col className={classes.Column} span={24} lg={{span: 8}}><YinYang/></Col>
-        <Col className={classes.Column} span={24} lg={{span: 8}}><Facebook/></Col>
-        <Col className={classes.Column} span={24} lg={{span: 8}}><StarFive/></Col>
-        <Col className={classes.Column} span={24} lg={{span: 8}}><BadgeRibbon/></Col>
 
         <Col className={classes.Column} span={24} lg={{span: 8}}><Leaf/></Col>
         <Col className={classes.Column} span={24} lg={{span: 8}}><Heart/></Col>
+        <Col className={classes.Column} span={24} lg={{span: 8}}><Moon/></Col>
+
+        <Col className={classes.Column} span={24} lg={{span: 8}}><Facebook/></Col>
+        <Col className={classes.Column} span={24} lg={{span: 8}}><StarFive/></Col>
+        <Col className={classes.Column} span={24} lg={{span: 8}}><BadgeRibbon/></Col>
 
     </Row>
 );
