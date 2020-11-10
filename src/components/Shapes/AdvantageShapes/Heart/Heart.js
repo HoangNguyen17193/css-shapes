1bbaf7158546ea109d5ef0995a9fdd17a3cc6634
hoangnguyen17193 <hoangnh17193@gmail.com>
2020-11-10 11:20:25 +0700

    display code

--- src/components/Shapes/AdvantageShapes/Heart/Heart.js
+++ src/components/Shapes/AdvantageShapes/Heart/Heart.js
@@ -2,12 +2,41 @@ import React from 'react';
 import classes from './Heart.module.scss';
 import Shape from '../../Shape/Shape';
 
-const Heart = () => (
-    <Shape title="Heart">
-        <div className={classes.Heart}>
-        </div>
-    </Shape>
+const Heart = () => {
+    const codeString =`
+    .Heart {
+      position: relative;
+      width: 120px;
+      height: 100px;
 
-);
+      &::before,
+      &:after {
+        position: absolute;
+        content: '';
+        left: 60px;
+        top: 0;
+        width: 60px;
+        height: 90px;
+        background: $primary-color;
+        border-radius: 60px 60px 0 0;
+        transform: rotate(-45deg);
+        transform-origin: 0 100%;
+      }
+
+      &::after {
+        left: 0;
+        transform: rotate(45deg);
+        transform-origin: 100% 100%;
+      }
+    }`;
+    return (
+        <Shape title="Heart" codeString={codeString}>
+            <div className={classes.Heart}>
+            </div>
+        </Shape>
+
+    );
+
+};
 
 export default Heart;
