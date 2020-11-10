c1625ce3ebe43534217c07d0def975d4a2919b5f
hoangnguyen17193 <hoangnh17193@gmail.com>
2020-11-10 11:17:11 +0700

    fixed height

--- src/components/Common/HighlightCode/HighLightCode.js
+++ src/components/Common/HighlightCode/HighLightCode.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { materialDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter';
+import {materialDark} from 'react-syntax-highlighter/dist/esm/styles/prism';
+
+import classes from './HighLightCode.module.scss';
 
 const HighLightCode = (props) => {
     return (
-        <SyntaxHighlighter language="scss" style={materialDark}>
-            {props.codeString || ''}
-        </SyntaxHighlighter>
+        <div className={classes.HighLightCode}>
+            <SyntaxHighlighter language="scss" style={materialDark}>
+                {props.codeString || ''}
+            </SyntaxHighlighter>
+        </div>
     )
 };
 
