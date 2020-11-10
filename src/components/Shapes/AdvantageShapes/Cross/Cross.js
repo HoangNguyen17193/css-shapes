e03eb9e26475434c171bacd412619165bafbc0f6
hoangnguyen17193 <hoangnh17193@gmail.com>
2020-11-10 11:09:15 +0700

    display code

--- src/components/Shapes/AdvantageShapes/Cross/Cross.js
+++ src/components/Shapes/AdvantageShapes/Cross/Cross.js
@@ -2,11 +2,30 @@ import React from 'react';
 import classes from './Cross.module.scss';
 import Shape from '../../Shape/Shape';
 
-const Cross = () => (
-    <Shape title='Cross'>
-        <div className={classes.Cross}>
-        </div>
-    </Shape>
-);
+const Cross = () => {
+    const codeString = `
+    .Cross {
+      background: $primary-color;
+      height: 75px;
+      position: relative;
+      width: 15px;
+      &::after {
+        background: $primary-color;
+        content: '';
+        height: 15px;
+        left: -30px;
+        position: absolute;
+        top: 30px;
+        width: 75px;
+      }
+    }
+    `;
+    return (
+        <Shape title='Cross' codeString={codeString}>
+            <div className={classes.Cross}>
+            </div>
+        </Shape>
+    );
+};
 
 export default Cross;
