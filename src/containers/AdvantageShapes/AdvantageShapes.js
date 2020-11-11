e2c68a174318bf71c76d01304b81c34d84513cb2
hoangnguyen17193 <hoangnh17193@gmail.com>
2020-11-11 17:12:48 +0700

    adjust layout

--- src/containers/AdvantageShapes/AdvantageShapes.js
+++ src/containers/AdvantageShapes/AdvantageShapes.js
@@ -4,13 +4,15 @@ import {Row, Col} from 'antd';
 import Hamburger from '../../components/Shapes/AdvantageShapes/Hamburger/Hamburger';
 import Clock from '../../components/Shapes/AdvantageShapes/Clock/Clock';
 import CoffeeCup from '../../components/Shapes/AdvantageShapes/CoffeeCup/CoffeeCup';
+import CaptainAmericaLogo from '../../components/Shapes/AdvantageShapes/CaptainAmericaLogo/CaptainAmericaLogo';
 
 const AdvantageShapes = () => {
     return (
         <Row>
-            <Col span={24} lg={{span: 16, offset: 4}}><Hamburger/></Col>
-            <Col span={24} lg={{span: 16, offset: 4}}><Clock/></Col>
+            <Col span={24} lg={{span: 12, offset: 6}}><Hamburger/></Col>
+            <Col span={24} lg={{span: 12, offset: 6}}><Clock/></Col>
             <Col span={24} lg={{span: 12, offset: 6}}><CoffeeCup/></Col>
+            <Col span={24} lg={{span: 12, offset: 6}}><CaptainAmericaLogo/></Col>
         </Row>
     );
 };
