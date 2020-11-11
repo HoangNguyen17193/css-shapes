6bd9efc3fe3f14f566109725269d456bca0f0c30
hoangnguyen17193 <hoangnh17193@gmail.com>
2020-11-11 15:46:48 +0700

    add constants

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+const ROUTES = {
+    home: {
+        path: '/',
+        key: 'home'
+    },
+    advantageShape: {
+        path: '/advantage-shapes',
+        key: 'advantage-shapes'
+    }
+};
+
+export {
+    ROUTES
+}
