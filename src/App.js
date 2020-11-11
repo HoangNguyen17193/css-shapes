69c9a1e924d7b7a2381ed49a3efed5863eccdfb6
hoangnguyen17193 <hoangnh17193@gmail.com>
2020-11-11 15:16:00 +0700

    define routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, {Component} from 'react';
-import MainLayout from "./components/Common/MainLayout/MainLayout";
-import BasicShapes from "./containers/BasicShapes/BasicShapes";
+import {Route, Switch} from 'react-router-dom';
+
+import MainLayout from './components/Common/MainLayout/MainLayout';
+import BasicShapes from './containers/BasicShapes/BasicShapes';
+import AdvantageShapes from './containers/AdvantageShapes/AdvantageShapes';
 
 class App extends Component {
     render() {
         return (
             <MainLayout>
-                <BasicShapes />
+                <Switch>
+                    <Route path='/' exact component={BasicShapes} />
+                    <Route path='/basic-shapes' component={BasicShapes} />
+                    <Route path='/advantage-shapes' component={AdvantageShapes} />
+                </Switch>
             </MainLayout>
         )
     }
