a3e55ef427dcbd8a36bae6046af0ee57f916d4fd
hoangnguyen17193 <hoangnh17193@gmail.com>
2020-11-12 14:19:13 +0700

    add shapes

--- src/containers/AdvantageShapes/AdvantageShapes.js
+++ src/containers/AdvantageShapes/AdvantageShapes.js
@@ -6,6 +6,7 @@ import Clock from '../../components/Shapes/AdvantageShapes/Clock/Clock';
 import CoffeeCup from '../../components/Shapes/AdvantageShapes/CoffeeCup/CoffeeCup';
 import CaptainAmericaLogo from '../../components/Shapes/AdvantageShapes/CaptainAmericaLogo/CaptainAmericaLogo';
 import Monitor from "../../components/Shapes/AdvantageShapes/Monitor/Monitor";
+import GitLogo from "../../components/Shapes/AdvantageShapes/GitLogo/GitLogo";
 
 const AdvantageShapes = () => {
     return (
@@ -15,6 +16,7 @@ const AdvantageShapes = () => {
             <Col span={24} lg={{span: 12, offset: 6}}><CoffeeCup/></Col>
             <Col span={24} lg={{span: 12, offset: 6}}><CaptainAmericaLogo/></Col>
             <Col span={24} lg={{span: 12, offset: 6}}><Monitor/></Col>
+            <Col span={24} lg={{span: 12, offset: 6}}><GitLogo/></Col>
         </Row>
     );
 };
